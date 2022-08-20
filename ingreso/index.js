f209360cdf13a7a95c62ec28dc919551046d61ce
Nicolas Ramirez <nicolasramirez22498@gmail.com>
2022-08-20 04:25:53 -0300

    se agregó REGISTRARSE, se modifico nav

--- ingreso/index.js
+++ ingreso/index.js
@@ -2,12 +2,12 @@ const toggleButton = document.getElementById("button-menu");
 const navWrapper = document.getElementById("nav");
 
 toggleButton.addEventListener("click", () => {
-    toggleButton.classList.toggle("close");
-    navWrapper.classList.toggle("show");
+  toggleButton.classList.toggle("close");
+  navWrapper.classList.toggle("show");
 });
 navWrapper.addEventListener("click", e => {
-    if (e.target.id === "nav") {
-      navWrapper.classList.remove("show");
-      toggleButton.classList.remove("close");
-    }
+  if (e.target.id === "nav") {
+    navWrapper.classList.remove("show");
+    toggleButton.classList.remove("close");
+  }
 });
